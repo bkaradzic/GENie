8e934709a5e753a05ef8feb9279a60caf44f82f8
Stuart Carnie <stuart.carnie@gmail.com>
2016-05-13 20:23:40 -0700

    local function

--- src/actions/xcode/_xcode.lua
+++ src/actions/xcode/_xcode.lua
@@ -5,6 +5,25 @@
 --
 
 	premake.xcode = { }
+	
+
+--
+-- Verify only single target kind for Xcode project
+--
+-- @param prj
+--    Project to be analyzed
+--
+
+	local function checkproject(prj)
+		-- Xcode can't mix target kinds within a project
+		local last
+		for cfg in premake.eachconfig(prj) do
+			if last and last ~= cfg.kind then
+				error("Project '" .. prj.name .. "' uses more than one target kind; not supported by Xcode", 0)
+			end
+			last = cfg.kind
+		end
+	end
 
 --
 -- Set default toolset
@@ -51,7 +70,7 @@
 			premake.clean.directory(prj, "%%.xcodeproj")
 		end,
 		
-		oncheckproject = xcode.checkproject,
+		oncheckproject = checkproject,
 	}
 
 	newaction 
@@ -93,7 +112,7 @@
 			premake.clean.directory(prj, "%%.xcworkspace")
 		end,
 		
-		oncheckproject = xcode.checkproject,
+		oncheckproject = checkproject,
 			for cfg in premake.eachconfig(prj) do
 				if last and last ~= cfg.kind then
 					error("Project '" .. prj.name .. "' uses more than one target kind; not supported by Xcode", 0)
