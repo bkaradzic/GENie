ad9d7c401647b1dcb4cb2935f73952e646aad516
Jan-Eric Duden <jeduden@gmail.com>
2016-05-15 22:52:06 +0200

    Also add support for the other build systems.

--- src/actions/fastbuild/fastbuild_project.lua
+++ src/actions/fastbuild/fastbuild_project.lua
@@ -47,6 +47,9 @@ local function compile(indentlevel, prj, cfg)
 	end
 
 	_p(indentlevel, ".IncludeDirs = ''")
+	for _, includedir in ipairs(cfg.userincludedirs) do
+		_p(indentlevel+1, "+ ' /I\"%s\"'", includedir)
+	end
 	for _, includedir in ipairs(cfg.includedirs) do
 		_p(indentlevel+1, "+ ' /I\"%s\"'", includedir)
 	end
