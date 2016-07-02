a15fd186cad4a74f2127e910b3d8c95ad73694d7
Tengato <pat.munns@gmail.com>
2016-07-01 18:02:49 -0700

    Adopted use of .CompilerInputFilesRoot property added by FASTBuild update.

--- src/actions/fastbuild/fastbuild_project.lua
+++ src/actions/fastbuild/fastbuild_project.lua
@@ -429,8 +429,7 @@ function premake.fastbuild.project(prj)
 	end
 	-- These lines will allow fastbuild to keep the object files in the right folder hierarchy
 	-- without including everything underneath.
-	_p(1, ".CompilerInputPath = '%s/'", commonbasepath)
-	_p(1, ".CompilerInputPattern = 'ignoreall'")
+	_p(1, ".CompilerInputFilesRoot = '%s/'", commonbasepath)
 	_p(1, '.CompilerInputFiles = {')
 	for _, file in ipairs(cppfiles) do
 		_p(2, "'%s',", file)
