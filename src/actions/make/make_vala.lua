55340802bfb19a6efcaac00a9b6b402fb48b9ee5
Daniele Bartolini <dbartolini.aa@gmail.com>
2017-03-27 20:02:05 +0200

    Add buildoptions_vala and vapidirs

--- src/actions/make/make_vala.lua
+++ src/actions/make/make_vala.lua
@@ -124,8 +124,9 @@
 		_p('  TARGETDIR  = %s', _MAKE.esc(cfg.buildtarget.directory))
 		_p('  TARGET     = $(TARGETDIR)/%s', _MAKE.esc(cfg.buildtarget.name))
 		_p('  DEFINES    +=%s', make.list(valac.getdefines(cfg.defines)))
+		_p('  VAPIDIRS   +=%s', make.list(valac.getvapidirs(cfg.vapidirs)))
 		_p('  PKGS       +=%s', make.list(valac.getlinks(cfg.links)))
-		_p('  FLAGS      += $(DEFINES) $(PKGS)%s', make.list(table.join(valac.getvalaflags(cfg), valac.getbuildoptions(cfg.buildoptions), valac.getbuildoptions(cfg.buildoptions_c))))
+		_p('  FLAGS      += $(DEFINES) $(VAPIDIRS) $(PKGS)%s', make.list(table.join(valac.getvalaflags(cfg), valac.getbuildoptions(cfg.buildoptions), valac.getbuildoptions(cfg.buildoptions_c), cfg.buildoptions_vala)))
 
 		_p('  define PREBUILDCMDS')
 		if #cfg.prebuildcommands > 0 then
