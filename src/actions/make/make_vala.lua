5ed8c20d9aac37aedb68ebde49dc95fa26c73526
Anders Stenberg <anders@Anderss-MacBook-Pro.local>
2018-03-06 12:19:59 +0100

    Made gmake escape dollar sign in build commands properly.

--- src/actions/make/make_vala.lua
+++ src/actions/make/make_vala.lua
@@ -135,21 +135,21 @@
 		_p('  define PREBUILDCMDS')
 		if #cfg.prebuildcommands > 0 then
 			_p('\t@echo Running pre-build commands')
-			_p('\t%s', table.implode(cfg.prebuildcommands, "", "", "\n\t"))
+			_p('\t%s', table.implode(cfg.prebuildcommands, "", "", "\n\t"):gsub("%$", "$$"))
 		end
 		_p('  endef')
 
 		_p('  define PRELINKCMDS')
 		if #cfg.prelinkcommands > 0 then
 			_p('\t@echo Running pre-link commands')
-			_p('\t%s', table.implode(cfg.prelinkcommands, "", "", "\n\t"))
+			_p('\t%s', table.implode(cfg.prelinkcommands, "", "", "\n\t"):gsub("%$", "$$"))
 		end
 		_p('  endef')
 
 		_p('  define POSTBUILDCMDS')
 		if #cfg.postbuildcommands > 0 then
 			_p('\t@echo Running post-build commands')
-			_p('\t%s', table.implode(cfg.postbuildcommands, "", "", "\n\t"))
+			_p('\t%s', table.implode(cfg.postbuildcommands, "", "", "\n\t"):gsub("%$", "$$"))
 		end
 		_p('  endef')
 
