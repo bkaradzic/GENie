0d5f974625fb07180842e481fe202cf1399b56d3
David Amador <david.djlink@gmail.com>
2019-05-15 15:53:33 +0100

    Added dpiawareness configuration, being set on Visual Studio project using <EnableDpiAwareness> tag

--- src/base/api.lua
+++ src/base/api.lua
@@ -734,6 +734,17 @@
 			kind = "filelist",
 			scope = "container",
 		},
+		
+		dpiawareness = 
+		{
+			kind = "string",
+			scope = "config",
+			allowed = {
+				"None",
+				"High",
+				"HighPerMonitor",
+			}
+		},
 
 		xcodeprojectopts =
 		{
