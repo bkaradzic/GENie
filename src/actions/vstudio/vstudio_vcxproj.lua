fb1cd455b922f70736b5cd964f47a98d4b8919c9
Bill Freist <billfreist@users.noreply.github.com>
2019-06-03 11:39:27 -0700

    Fix copy/pasta and actually set the correct VCProjectVersion property value

--- src/actions/vstudio/vstudio_vcxproj.lua
+++ src/actions/vstudio/vstudio_vcxproj.lua
@@ -117,7 +117,7 @@
 
 		local is2019 = premake.action.current() == premake.action.get("vs2019")
 		if is2019 then
-		    _p(2, '<VCProjectVersion>%s</VCProjectVersion>', premake.vstudio.toolset)
+		    _p(2, '<VCProjectVersion>%s</VCProjectVersion>', premake.vstudio.toolsVersion)
 		end
 		_p(2, '<ConfigurationType>%s</ConfigurationType>', vc2010.config_type(cfg))
 		_p(2, '<UseDebugLibraries>%s</UseDebugLibraries>', iif(optimisation(cfg) == "Disabled","true","false"))
